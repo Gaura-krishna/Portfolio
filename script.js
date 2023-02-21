5a39ca2ba79a9af7b4fcdfbf2f3fd67037cce032
Gaura-krishna <118505849+Gaura-krishna@users.noreply.github.com>
2023-02-21 23:59:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // console.log("hi")
 function typing(){
     var typed = new Typed('.auto-type', {
-        strings:["A Front-End Web Developer passionate about creating interactive applicationsand experiences on the web."],
+        strings:["A Front-End Web Developer passionate about creating interactive on the websites."],
         typeSpeed:80,
         // backSpeed:150,
         // loop:true
