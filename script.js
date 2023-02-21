2a4d84fc0c45004db5feea99dc9a3f28b111c633
Gaura-krishna <118505849+Gaura-krishna@users.noreply.github.com>
2023-02-21 12:48:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// console.log("hi")
+function typing(){
+    var typed = new Typed('.auto-type', {
+        strings:["A Front-End Web Developer passionate about creating interactive applicationsand experiences on the web."],
+        typeSpeed:80,
+        // backSpeed:150,
+        // loop:true
+    })
+}
+typing();
